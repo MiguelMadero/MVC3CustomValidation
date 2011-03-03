a12451b9ae7aa86d8bd075ba3869c52067626d7d
Robert Moore <rob@mooredesign.com.au>
2011-03-03 23:47:01 +0800

    Adding initial project files

---
+++ CustomValidation/MetadataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using CustomValidation.Controllers;
+
+namespace CustomValidation
+{
+	public class MetadataProvider : DataAnnotationsModelMetadataProvider
+	{
+		protected override ModelMetadata CreateMetadata(IEnumerable<Attribute> attributes, Type containerType,
+			Func<object> modelAccessor, Type modelType, string propertyName)
+		{
+			// Default metadata implementations
+			var metadata = base.CreateMetadata(attributes, containerType, modelAccessor, modelType, propertyName);
+
+			// Auto-sentence case for display name
+			if (typeof(ISimpleType).IsAssignableFrom(metadata.ModelType))
+			{
+				metadata.DisplayName = metadata.PropertyName;
+			}
+
+			return metadata;
+		}
+	}
+}
